05b68094d5d1364fe01eac4e753eb072c0067110
sama.aloda <alodas@mcmaster.ca>
2025-03-22 14:35:14 -0400

    mission report reports creek & esite only if found

--- src/main/java/ca/mcmaster/se2aa4/island/team44/MissionReport.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team44/MissionReport.java
@@ -11,13 +11,14 @@ public class MissionReport{
     }
     public String generateReport(){
         StringBuilder mission = new StringBuilder();
-        if(d.sufficientBattery()){
+        if(d.sufficientBattery()&&d.ifPossiblyFound()){
             mission.append("Creek ID:").append(d.getClosestCreek().getID()).append("\n");
             mission.append("Emergency Site ID:").append(d.getESite().getID()).append("\n");
             //logger.info(d.getClosestCreek().getLocation());
             //logger.info(d.getESite().getLocation());  
 
-        }else mission.append("Insufficent Battery: Return to Base");
+        }else if(!d.sufficientBattery()) mission.append("Insufficent Battery: Return to Base");
+        else mission.append("Creek and Emergency Site were not found: Return to Base");
 
        // logger.info("***Mission Report" + mission.toString()); 
         return mission.toString();
